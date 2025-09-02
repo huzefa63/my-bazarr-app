2c8477d9b26ea97e936025fe86daf13e316e403b
unknown <huzefaratlam63@gmail.com>
2025-09-02 11:37:56 +0530

    fixed another import

--- app/page.js
+++ app/page.js
@@ -1,10 +1,6 @@
-import { SignIn,SignInButton,SignUpButton,UserButton,UserProfile } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
-import Image from "next/image";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const {userId} = await auth();
-  if(userId) redirect('/app/browse');
-  else redirect('/home');
+  redirect('/app/browse');
+
 }
