8103ac4f63667f8b06ba58c7a2c119477bfcce16
unknown <huzefaratlam63@gmail.com>
2025-09-03 13:59:31 +0530

    fixed a bug

--- actions/user.js
+++ actions/user.js
@@ -8,7 +8,8 @@ export async function getOtp(email){
     const cookie = await cookies();
     const res = await fetch(`http://localhost:5000/user/verifyEmail`, {
         method:'POST',
-        headers:{'Content-Type':'application/json',Cookie:`jwt=${cookie.get('jwt').value}`},
+        headers:{'Content-Type':'application/json'},
+        // headers:{'Content-Type':'application/json',Cookie:`jwt=${cookie.get('jwt').value}`},
         body:JSON.stringify({email}),
         
     });
@@ -20,7 +21,7 @@ export async function verifyOtp(email,name,otp){
     const cookie = await cookies();
     const res = await fetch(`http://localhost:5000/user/createUser`, {
         method:'POST',
-        headers:{'Content-Type':'application/json',Cookie:`jwt=${cookie.get('jwt').value}`},
+        headers:{'Content-Type':'application/json'},
         body:JSON.stringify({email,username:name,otp}),
     });
     const resJson = await res.json();
